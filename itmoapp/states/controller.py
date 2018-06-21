7d12b58ce786fa8b8b40bcacf67eca039f198c3d
Taly <vitalik7tv@yandex.ru>
2018-06-21 15:05:01 +0300

    save

--- itmoapp/states/controller.py
+++ itmoapp/states/controller.py
@@ -13,7 +13,96 @@ class Controller:
         # Dict of available states
         self.states_list = {}
 
-    async def get(self, payload):
+    async def goto(self, payload, state_name=None, data=None):
+        """
+        Change state and call its before function
+
+        Example:
+        > return await self.controller.goto(payload, 'auth', {'login': 'user273'})
+
+        :param dict payload:
+        :param string state_name:
+        :param dict data:
+        :return void:
+        """
+        self.sdk.log("GOTO state {}".format(state_name))
+
+        # Update state
+        self.__set(payload, state=state_name, data=data)
+
+        if state_name is not None:
+            # Call state's before function
+            self.get_state_class(state_name).before(payload, data)
+
+            # Call wait user answer
+            await self.sdk.broker.api.wait_user_answer(
+                payload['user'],
+                payload['chat']
+            )
+
+    async def process(self, payload):
+        """
+        Trigger process() command for current state
+
+        :param dict payload:
+        :return:
+        """
+        # Get current state from DB
+        state = self.__get(payload)
+        self.sdk.log("Process state {} for chat {}".format(state['name'], payload['chat']))
+
+        # If state name is missing the return null
+        if state['name'] is None:
+            return
+
+        # Find state class in map
+        state_class = self.get_state_class(state['name'])
+
+        # Call process function for target state
+        await state_class.process(payload, state['data'])
+
+    async def reenter(self, payload):
+        """
+        Reenter to current state
+
+        :param payload:
+        :return:
+        """
+        # Get current state from DB
+        current_state = self.__get(payload)
+
+        # Do nothing if current state in none
+        if current_state is None:
+            return
+
+        # Enter to current state again
+        await self.goto(payload, current_state['name'], current_state['data'])
+
+    def is_state_exist(self, name):
+        """
+        Check for a state existing
+
+        :param string name:
+        :return boolean:
+        """
+        # Check for a state with target name existing in a states_list dict
+        return name in self.states_list
+
+    def get_state_class(self, name):
+        """
+        Get state class helper if it is exists
+
+        :param string name: state name
+        :return:
+        """
+        # If state is not exist then throw an exception
+        if not self.is_state_exist(name):
+            raise Exception('Can not find class for state {}'.format(name))
+
+        # Otherwise return state class
+        return self.states_list[name](self)
+
+    def __get(self, payload):
         """
         Get state from DB for target chat
 
@@ -33,7 +122,7 @@ class Controller:
         # Return current state data
         return current_state
 
-    async def set(self, payload, state=None, data=None):
+    def __set(self, payload, state=None, data=None):
         """
         Set state for target chat
 
@@ -77,92 +166,3 @@ class Controller:
             # Upsert = true
             True
         )
-
-    async def goto(self, payload, state_name=None, data=None):
-        """
-        Change state and call its before function
-
-        Example:
-        > return await self.controller.goto(payload, 'auth', {'login': 'user273'})
-
-        :param dict payload:
-        :param string state_name:
-        :param dict data:
-        :return void:
-        """
-        self.sdk.log("GOTO state {}".format(state_name))
-
-        # Update state
-        await self.set(payload, state=state_name, data=data)
-
-        if state_name is not None:
-            # Call state's before function
-            await self.get_state_class(state_name).before(payload, data)
-
-            # Call wait user answer
-            await self.sdk.broker.api.wait_user_answer(
-                payload['user'],
-                payload['chat']
-            )
-
-    async def process(self, payload):
-        """
-        Trigger process() command for current state
-
-        :param dict payload:
-        :return:
-        """
-        # Get current state from DB
-        state = await self.get(payload)
-        self.sdk.log("Process state {} for chat {}".format(state['name'], payload['chat']))
-
-        # If state name is missing the return null
-        if state['name'] is None:
-            return
-
-        # Find state class in map
-        state_class = self.get_state_class(state['name'])
-
-        # Call process function for target state
-        await state_class.process(payload, state['data'])
-
-    async def reenter(self, payload):
-        """
-        Reenter to current state
-
-        :param payload:
-        :return:
-        """
-        # Get current state from DB
-        current_state = await self.get(payload)
-
-        # Do nothing if current state in none
-        if current_state is None:
-            return
-
-        # Enter to current state again
-        await self.goto(payload, current_state['name'], current_state['data'])
-
-    def is_state_exist(self, name):
-        """
-        Check for a state existing
-
-        :param string name:
-        :return boolean:
-        """
-        # Check for a state with target name existing in a states_list dict
-        return name in self.states_list
-
-    def get_state_class(self, name):
-        """
-        Get state class helper if it is exists
-
-        :param string name: state name
-        :return:
-        """
-        # If state is not exist then throw an exception
-        if not self.is_state_exist(name):
-            raise Exception('Can not find class for state {}'.format(name))
-
-        # Otherwise return state class
-        return self.states_list[name](self)
